a66efc77e369b493b2a5dc53d1e3294b7cc1e35d
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-10-31 22:02:24 -0300

    data and first route

--- src/app.js
+++ src/app.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => res.status(200).send('<h1>Data will be here soon...</h1>'));
+
 module.exports = app;
