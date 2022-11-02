95cf14810f81b088cb0c337b13b3f4008284786c
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-11-01 22:22:59 -0300

    started developing api's post method

--- src/app.js
+++ src/app.js
@@ -9,10 +9,13 @@ app.get('/', async (req, res) => res.status(200).send(await getUsers()));
 
 app.get('/users/:id', async (req, res) => {
   const { id } = req.params;
-  console.log(id);
   const users = await getUsers();
   const requestedUser = users.find((user) => user.id === Number(id));
   res.status(200).send(requestedUser);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).send('Data will be here soon...');
 });
 
 module.exports = app;
--- tests/sandboxApi.test.js
+++ tests/sandboxApi.test.js
@@ -8,25 +8,38 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testing if the API works properly', function () {
-  it('Using GET method on the root route', async function () {
-    const users = await getUsers();
-
-    const response = await chai.request(app).get('/');
-
-    expect(response.status).to.be.equal(200);
-    expect(response.body).to.deep.equal(users);
+  describe('GET method', function () {
+    it('Using GET method on the root route', async function () {
+      const users = await getUsers();
+  
+      const response = await chai.request(app).get('/');
+  
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal(users);
+    });
+    it('Using GET method to find a person by an id', async function () {
+      const neron = {
+        id: 10,
+        firstName: 'Neron',
+        lastName: 'Izakovitz',
+        gender: 'Male',
+      };
+  
+      const response = await chai.request(app).get('/users/10');
+  
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.deep.equal(neron);
+    });
   });
-  it('Using GET method to find a person by an id', async function () {
-    const neron = {
-      id: 10,
-      firstName: 'Neron',
-      lastName: 'Izakovitz',
-      gender: 'Male',
-    };
+  describe('POST method', function () {
+    it('Using POST method to add a new user', async function () {
+      // Mock fsWriteFile
 
-    const response = await chai.request(app).get('/users/10');
+      // Create new user
 
-    expect(response.status).to.be.equal(200);
-    expect(response.body).to.deep.equal(neron);
+      // Post new user 
+
+      // Assert database's length and the last item's id
+    });
   });
 });
